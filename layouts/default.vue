d59b3e6f1148600df5aebecf53642a8f4ca6878d
Djoumessi-Aurel <aurelbazir@gmail.com>
2022-08-22 11:43:10 +0100

    Initialisation du projet (avec Nuxt3)

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+<div>
+  <nav><h2 class="text-lg bg-red-300 font-serif">Voici ma navbar. ça apparaît partout.</h2></nav>
+  <slot />
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>LA GERTA</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data(){
+    return {
+    };
+  }
+};
+</script>
+
+<style>
+a.nuxt-link-active {
+  font-weight: bold;
+}
+/* exact link will show the primary color for only the exact matching link */
+a.nuxt-link-exact-active {
+  color: #00c58e;
+}
+</style>
