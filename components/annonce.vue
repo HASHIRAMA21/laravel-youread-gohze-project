1aaee579c33bfd6f1fc7739e015070a627224ace
Richelle7 <richellemayap@gmail.com>
2022-08-23 09:50:06 +0100

    version avec ajout de la page

---
+++ components/annonce.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="cursor-pointer w-full flex mt-4 annonce">
+        <div class="rounded-full w-16 h-16 ml-11  justify-center ">
+            <img :src="imgUrl" class="w-full h-full object-cover" />
+        </div>
+        <div class="content">
+            <p>{{ name }}</p>
+            <p class="text-gray text-xs float-left mt-4">{{ nbCommentaire }} commentaires</p>
+        </div>
+    </div>
+</template>
+<style scoped>
+.annonce:hover{
+    -webkit-transform: scale(1.08);
+    transform: scale(1.08);
+    -webkit-transition: 0.7s ease-in-out;
+    transition: 0.7s ease-in-out;
+    opacity: 1;
+    /* background-color: black; */
+    color:#007D74;
+    font-weight: bold;
+   
+}
+ .content{
+       margin: auto;
+       margin-left: 36px;
+    }
+</style>
+<script>
+export default {
+    name: "annonce",
+    props: {
+        "name": "",
+        "imgUrl": "",
+        "nbCommentaire": ""
+    }
+}
+</script>
---
+++ components/categorie.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="shadow-card grid grid-cols-2 bg-pink">
+        <div class="">
+            <p class="font-semibold ml-3.5 w-full">{{ name }}</p>
+        </div>
+        <div class="">
+            <img :src="imageUrl" alt="image">
+        </div>
+    </div>
+</template>
+<style scoped>
+p {
+    margin-top: 50%;
+    margin-bottom: 50%;
+}
+
+.shadow-card {
+    box-shadow: 0px 2px 10px 0px #00000040;
+    width: 255px;
+    height: 150px;
+    border-radius: 15px;
+}
+
+.shadow-card {
+    overflow: hidden
+}
+
+/* .shadow-card {width:100%;height:auto} */
+.shadow-card:hover {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+    -webkit-transition: 0.7s ease-in-out;
+    transition: 0.7s ease-in-out;
+    opacity: 0.8;
+    background-color: #0d3330;
+    color:white
+}
+
+img {
+    width: 100%;
+    height: 100%;
+}
+
+.bg-pink {
+    background-color: #FFFDF9;
+    height: 100%;
+}
+</style>
+<script>
+export default {
+    name: "Categorie",
+    props: {
+        "name": "",
+        "imageUrl": ""
+    },
+}
+</script>
