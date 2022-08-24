b5e2792656e2937bef3e14627b89c00ff7762f76
richelle7 <richellemayap@gmail.com>
2022-08-24 23:25:05 +0100

    version avec mise a jour du mode telephone

--- components/annonce.vue
+++ components/annonce.vue
@@ -1,38 +1,41 @@
 <template>
-    <div class="cursor-pointer w-full flex mt-4 annonce">
-        <div class="rounded-full lg:w-16 md:w-12 md:h-12 lg:h-16 lg:ml-11 ml-7  justify-center ">
-            <img :src="imgUrl" class="w-full h-full object-cover" />
-        </div>
-        <div class="content lg:text-base md:text-sm">
-            <p>{{ name }}</p>
-            <p class="text-gray text-xs float-left mt-4">{{ nbCommentaire }} commentaires</p>
-        </div>
+  <div class="cursor-pointer w-full flex mt-4 annonce">
+    <div class="rounded-full w-16 h-16 lg:ml-11 ml-7 justify-center">
+      <img :src="imgUrl" class="w-full object-cover" />
     </div>
+    <div class="content m-text-bigger-normal ml-5 lg:ml-9">
+      <p>{{ name }}</p>
+      <p class="text-gray m-text-extra-small float-left mt-4">
+        {{ nbCommentaire }} commentaires
+      </p>
+    </div>
+  </div>
 </template>
 <style scoped>
-.annonce:hover{
+@media only screen and (min-width: 640px) {
+  .annonce:hover {
     -webkit-transform: scale(1.08);
     transform: scale(1.08);
     -webkit-transition: 0.7s ease-in-out;
     transition: 0.7s ease-in-out;
     opacity: 1;
     /* background-color: black; */
-    color:#007D74;
+    color: #007d74;
     font-weight: bold;
-   
+  }
 }
- .content{
-       margin: auto;
-       margin-left: 36px;
-    }
+/* .content{
+       /* margin: auto; */
+/* margin-left: 36px; */
+/* } */
 </style>
 <script>
 export default {
-    name: "annonce",
-    props: {
-        "name": "",
-        "imgUrl": "",
-        "nbCommentaire": ""
-    }
-}
+  name: "annonce",
+  props: {
+    name: "",
+    imgUrl: "",
+    nbCommentaire: "",
+  },
+};
 </script>
--- pages/app/listAnnonces.vue
+++ pages/app/listAnnonces.vue
@@ -1,114 +1,152 @@
 <template>
-    <div class="">
-        <div class="flex mt-16 w-full justify-center">
-            <div class="">
-                <div class="bg-white rounded-2xl w-64 lg:w-64 md:w-52 md:h-44 lg:h-56 h-56 container disappear-bloc">
-                    <div class="grid">
-                        <p class="uppercase text-center text-indigo font-bold mb-6 mt-8 w-full">annonces</p>
-                        <p class="uppercase text-center w-40  m-auto"> vous avez une nouvelle annonce</p>
-                    </div>
-                </div>
-                <div class="disappear-bloc">
-                    <div class="">
-                        <figure class="circle-md bg-indigo figure-indigo"></figure>
-                    </div>
-                    <div>
-                        <figure class="circle-xl  bg-indigo-light mt-56 ml-32"></figure>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-white rounded-2xl container ml-12 content flex ">
-                <div class="w-full">
-                    <div v-for="(annonce, index) in annonces" :key="index" >
-                        <annonce :name="annonce.name" :imgUrl="annonce.imgUrl" :nbCommentaire="annonce.nbCommentaire" />
-                        <hr class="mt-4">
-                    </div>
-
-                </div>
-            </div>
-            <div class="disappear">
-                <div class="">
-                    <figure class="circle-xl  bg-orange mt-36 ml-36"></figure>
-                </div>
-                <div>
-                    <figure class="circle-sm bg-indigo-light mt-72 ml-40"></figure>
-                </div>
-            </div>
-        </div>
-
+  <div class="">
+    <div class="sm:hidden title-phone text-base grid">
+      <span class="m-auto">vous avez une nouvelle annonce</span>
     </div>
+    <div class="flex mt-5 sm:mt-16 w-full justify-center">
+      <div class="">
+        <div
+          class="
+            bg-white
+            rounded-2xl
+            w-64
+            lg:w-64
+            md:w-52 md:h-44
+            lg:h-56
+            h-56
+            container
+            disappear-bloc
+          "
+        >
+          <div class="grid">
+            <p
+              class="
+                uppercase
+                text-center text-indigo
+                font-bold
+                mb-6
+                mt-8
+                w-full
+                m-text-big
+              "
+            >
+              annonces
+            </p>
+            <p class="uppercase text-center w-40 m-auto m-text-normal">
+              vous avez une <span class="leading-8">nouvelle annonce </span>
+            </p>
+          </div>
+        </div>
+        <div class="disappear-bloc">
+          <div class="">
+            <figure class="circle-md bg-indigo figure-indigo"></figure>
+          </div>
+          <div>
+            <figure class="circle-xl bg-indigo-light mt-56 ml-32"></figure>
+          </div>
+        </div>
+      </div>
+      <div class="bg-white rounded-2xl container ml-12 content flex">
+        <div class="w-full">
+          <div v-for="(annonce, index) in annonces" :key="index">
+            <annonce
+              :name="annonce.name"
+              :imgUrl="annonce.imgUrl"
+              :nbCommentaire="annonce.nbCommentaire"
+            />
+            <hr class="mt-4" />
+          </div>
+        </div>
+      </div>
+      <div class="disappear">
+        <div class="">
+          <figure class="circle-xl bg-orange mt-36 ml-36"></figure>
+        </div>
+        <div>
+          <figure class="circle-sm bg-indigo-light mt-72 ml-40"></figure>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
-@media only screen and (max-width: 1024px) {
-   .disappear{ display: none ; }
-}
-@media only screen and (max-width: 640px) {
-   .disappear-bloc{ display: none ; }
-}
 .container {
-    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
 }
 
 .figure-indigo {
-    margin-top: 57%;
-    margin-left: 50%;
+  margin-top: 57%;
+  margin-left: 50%;
 }
 
-/* .figure-orange{
-        margin-top: 50%;
-        margin-left: 30%;
-    } */
-@media only screen and (min-width: 1024px) {
-    .content {
-        width: 688px;
-        height: 100%;
-    }
-}
-@media only screen and (min-width: 768px) {
-    .content {
-        width: 488px;
-        height: 100%;
-    }
+@media only screen and (max-width: 1024px) {
+  .disappear {
+    display: none;
+  }
 }
 @media only screen and (max-width: 640px) {
-    .content {
-        width: 320px;
-        height: 100%;
-        margin: auto;
-    }
+  .disappear-bloc {
+    display: none;
+  }
+}
+@media only screen and (min-width: 1024px) {
+  .content {
+    width: 688px;
+    height: 100%;
+  }
+}
+@media only screen and (min-width: 640px) and (max-width: 1023px) {
+  .content {
+    width: 488px;
+    height: 100%;
+  }
+}
+@media only screen and (max-width: 640px) {
+  .content {
+    width: 320px;
+    height: 100%;
+    margin: auto;
+  }
+  .title-phone {
+    margin: auto;
+    width: 320px;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+    height: 52px;
+    margin-top: 50px;
+  }
 }
 </style>
 <script>
-import annonce from '../../components/annonce.vue';
-export default ({
-    name: "annonces",
-    components: { annonce },
-    data() {
-        return {
-            annonces: [
-                {
-                    name: "Recherche d'un critique pour notre livre surnaturel, il doit etre...",
-                    imgUrl: '../../assets/img/annonces/Ellipse 49.png',
-                    nbCommentaire: 21
-                },
-                {
-                    name: "Recherche d'un critique pour notre livre surnaturel, il doit etre...",
-                    imgUrl: '../../assets/img/annonces/Ellipse 50.png',
-                    nbCommentaire: 21
-                },
-                {
-                    name: "Recherche d'un critique pour notre livre surnaturel, il doit etre...",
-                    imgUrl: '../../assets/img/annonces/Ellipse 51.png',
-                    nbCommentaire: 21
-                },
-                {
-                    name: "Recherche d'un critique pour notre livre surnaturel, il doit etre...",
-                    imgUrl: '../../assets/img/annonces/Ellipse 52.png',
-                    nbCommentaire: 21
-                },
-
-            ]
-        }
-    }
-})
+import annonce from "../../components/annonce.vue";
+export default {
+  name: "annonces",
+  components: { annonce },
+  data() {
+    return {
+      annonces: [
+        {
+          name: "Recherche d'un critique pour notre livre surnaturel",
+          imgUrl: "../../assets/img/annonces/Ellipse 49.png",
+          nbCommentaire: 21,
+        },
+        {
+          name: "Recherche d'un critique pour notre livre surnaturel",
+          imgUrl: "../../assets/img/annonces/Ellipse 50.png",
+          nbCommentaire: 21,
+        },
+        {
+          name: "Recherche d'un critique pour notre livre surnaturel",
+          imgUrl: "../../assets/img/annonces/Ellipse 51.png",
+          nbCommentaire: 21,
+        },
+        {
+          name: "Recherche d'un critique pour notre livre surnaturel",
+          imgUrl: "../../assets/img/annonces/Ellipse 52.png",
+          nbCommentaire: 21,
+        },
+      ],
+    };
+  },
+};
 </script>
