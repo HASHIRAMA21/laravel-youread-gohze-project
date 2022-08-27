c1c6e49764493612fe6dab471eb61d70a434b69e
richelle7 <richellemayap@gmail.com>
2022-08-27 13:22:11 +0100

    version avec ajout des fontawesome

--- pages/app/listChapitres.vue
+++ pages/app/listChapitres.vue
@@ -34,9 +34,10 @@
             <div v-for="(chap , index) in chapitres" :key="index">
                 <chapitre :nom="chap.nom" :numero="index+1" :dateCreation="chap.dateCreation" :statut="chap.statut" />
                 <div class="mt-5"><hr></div>
+                <p> <i class="fa-brands fa-2x fa-twitter"></i></p>
             </div>
         </div>
-         <font-awesome-icon icon="fa-solid fa-user-secret" />
+         <p> <i class="fa-brands fa-2x fa-twitter"></i></p>
       </div>
     </div>
   </div>
@@ -64,24 +65,7 @@ button {
 </style>
 <script>
 import chapitre from '../../components/annonce.vue'
-import { createApp } from 'vue'
-import App from './App.vue'
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
-/* add icons to the library */
-library.add(faUserSecret)
-
-createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon)
-.mount('#app')
 export default {
     name:"listChapitres",
     components:{
