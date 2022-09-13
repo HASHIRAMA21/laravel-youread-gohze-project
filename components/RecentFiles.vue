b8b07913aaa0be7414d377633a1c1defe230752b
Djoumessi <aurelbazir@gmail.com>
2022-09-13 05:03:18 +0100

    Fusion des travaux de Djoumessi sur la branche dev

---
+++ components/RecentFiles.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="recentFiles flex flex-col bg-white mt-4 md:mt-6 mb-10 overflow-auto rounded-md
+            px-2 md:px-4 lg:px-8 pt-2 md:pt-4 pb-8 md:pb-10">
+            <div class="font-bold md:text-lg mb-2">
+                Recent files
+            </div>
+            <div class="tableau">
+                <table class="w-full text-sm lg:text-base">
+                    <thead>
+                        <tr class="text-left">
+                            <th>Libellé</th><th>Date de création</th>
+                            <th>Date de modification</th><th class="images">Participants</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(file, index) in files" :key="index">
+                            <td>{{file.libelle}}</td>
+                            <td>{{file.dateCreation}}</td>
+                            <td>{{file.dateModif}}</td>
+                            <td class="images"><ProjectMembers :showText="false" :imgWidth="'36'" :decalage="9" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+ 
+            </div>
+    </div>
+</template>
+<script>
+import {ref} from 'vue';
+
+export default {
+  setup() {
+    const files = ref([
+      {libelle: 'Chapitre 1: The last man', dateCreation: '01 Juillet 2022', dateModif: '03 Juillet 2022'},
+      {libelle: 'Chapitre 2: The last man', dateCreation: '01 Juillet 2022', dateModif: '03 Juillet 2022'},
+      {libelle: 'Chapitre 3: The last man', dateCreation: '01 Juillet 2022', dateModif: '03 Juillet 2022'},
+      {libelle: 'Chapitre 4: The last man', dateCreation: '01 Juillet 2022', dateModif: '03 Juillet 2022'},
+      {libelle: 'Chapitre 5: The last man', dateCreation: '01 Juillet 2022', dateModif: '03 Juillet 2022'},
+      {libelle: 'Chapitre 6: The last man', dateCreation: '01 Juillet 2022', dateModif: '03 Juillet 2022'},
+    ]);
+
+    return { files };
+  }
+};
+</script>
+<style scoped>
+/* th.images, td.images{max-width: 150px;} */
+@media screen and (min-width: 640px) {
+  .recentFiles {
+    @apply mx-2 sm:mx-3 md:mx-5;
+  }
+}
+
+@media screen and (max-width: 640px) {
+th.images, td.images{max-width: 100px;}
+.tableau{min-width: 420px;}
+}
+</style>
---
+++ pages/dashboard/members.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="mt-12 dashboard-content-margin">
+    <div class="title">
+      <p class="m-text-bigger-normal uppercase w-full font-bold">
+        Liste des membres
+      </p>
+      <p class="m-text-small mt-2 font-semibold">
+        <nuxt-link to="/" class="text-indigo"> Home / </nuxt-link>
+        <span class="text-indigo">Projects</span>
+      </p>
+    </div>
+    <div class="contenant w-full bg-white mt-10">
+      <div class="flex flex-row-reverse">
+        <button class="text-white m-text-small mt-5 md:mt-7 lg:mt-9 mr-6 md:mr-9"
+        v-on:click="toggleModaleAdd">
+          <img
+            src="../../assets/img/plus.png"
+            class="inline-block"
+          />
+          <span class="ml-1">New Member</span>
+        </button>
+      </div>
+      <div class="p-4 md:p-8">
+        <div class=""><p class="uppercase font-semibold text-gray-600 mb-1">Liste des membres</p></div>
+        <MemberTable ref="MemberTable" />
+      </div>
+    </div>
+    <CreateMemberModal :showModal="showModalAdd" :closeFunction="toggleModaleAdd"
+     @create-member="createItem" />
+    
+  </div>
+</template>
+
+<script>
+definePageMeta({ layout: 'dashboard' });
+
+import {ref} from 'vue';
+
+export default {
+
+  setup(){
+    const showModalAdd = ref(false);
+    const MemberTable = ref(null);
+
+    function toggleModaleAdd(){
+      showModalAdd.value = !showModalAdd.value;
+    }
+
+    function createItem(item){
+      MemberTable.value.createItem(item);
+      toggleModaleAdd();
+    }
+
+    return {MemberTable, showModalAdd, toggleModaleAdd, createItem};
+  }
+};
+</script>
+
+<style scoped>
+.contenant {
+  border-radius: 10px;
+  box-shadow: 0px 1px 10px rgba(28, 201, 189, 0.29);
+  margin-bottom: 50px;
+  /* margin: auto; */
+}
+button {
+  background: linear-gradient(87.37deg, #148b77 3.62%, #6dc885 97.68%);
+  border-radius: 9px;
+  padding: 8px 10px 8px 10px;
+}
+</style>
